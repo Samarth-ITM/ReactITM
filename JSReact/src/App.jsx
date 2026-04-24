2f373eac18a5e5e8ae835a50444f88c000e61435
Samarth <2025.samarths@isu.ac.in>
2026-04-24 13:52:13 +0530

    apps

--- JSReact/src/App.jsx
+++ JSReact/src/App.jsx
@@ -2,9 +2,12 @@ import HookEx from './components/One';
 
 const App = () => {
   return (
-    <div>
-      <HookEx />
-    </div>
+    <>
+      <div className="App flex gap-10 p-10" >
+        <h1 className="text-2xl font-bold">React Hooks</h1>
+        <HookEx />
+      </div>
+    </>
   )
 }
 
